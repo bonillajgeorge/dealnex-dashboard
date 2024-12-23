833f83dbe2a3d6eb02425e8c82bdd0b9529c96b5
George J. Bonilla <bonillajgeorge@gmail.com>
2024-12-22 19:59:20 -0500

    Add files via upload

---
+++ tailwind.config.ts
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./components/ui/{button.tsx,dropdown-menu}.tsx",
+    "./app/welcome-page/page.tsx",      
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
